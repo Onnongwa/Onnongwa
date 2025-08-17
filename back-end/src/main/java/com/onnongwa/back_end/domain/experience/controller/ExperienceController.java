700afd7b3a1cc93d07f2426e4c9486e49e19c55b
jjsh0208 <jjsh0208@gmail.com>
2025-08-17 20:46:21 +0900

    feat : ExperienceController 생성

---
+++ back-end/src/main/java/com/onnongwa/back_end/domain/experience/controller/ExperienceController.java
@@ -0,0 +1,15 @@
+package com.onnongwa.back_end.domain.experience.controller;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/experience")
+public class ExperienceController {
+
+}
