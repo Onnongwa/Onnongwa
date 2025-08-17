85a62c6a183417a2b8362a97b867ea83a2bb0ebb
jjsh0208 <jjsh0208@gmail.com>
2025-08-17 22:25:35 +0900

    feat : <Back> Experience 구조 변경
    - 체험 스케줄 목록을 정규화해 별도 테이블로 분리

--- back-end/src/main/java/com/onnongwa/back_end/domain/experience/controller/ExperienceController.java
+++ back-end/src/main/java/com/onnongwa/back_end/domain/experience/controller/ExperienceController.java
@@ -5,13 +5,14 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.onnongwa.back_end.domain.experience.controller.dto.ExpRegisterDTO;
 import com.onnongwa.back_end.domain.experience.service.ExperienceService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,21 @@ public class ExperienceController {
 
 	private final ExperienceService experienceService;
 
+	@PostMapping("/onboarding")
+	public ResponseEntity<?> generateExperienceContent(@RequestBody ExpRegisterDTO dto){
+
+		experienceService.generateExperienceContent(dto);
+
+		return ResponseEntity.ok(null);
+
+	}
+
+
 	@PostMapping("/imgupload")
-	public  ResponseEntity<?> imageUpload(@RequestParam("file")MultipartFile file) throws IOException {
+	public ResponseEntity<?> imageUpload(@RequestParam("file")MultipartFile file) throws IOException {
 
 		String url = experienceService.saveImageAndGetUrl(file);
 
-		System.out.println("test : " + url);
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(Map.of("url", url));
 	}
