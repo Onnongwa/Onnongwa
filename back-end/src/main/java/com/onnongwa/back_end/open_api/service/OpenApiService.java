dfd1c191aae63da1ae6fc1142699d60499f70eb4
jjsh0208 <jjsh0208@gmail.com>
2025-08-19 17:56:48 +0900

    feat : <Back> ai 체험 생성 온보딩 요청 사항 수정
    - 빠진 부분들을 추가해줬고 하이라이트, 포함사항, 해시태그 추출의 내용을 추가함

--- back-end/src/main/java/com/onnongwa/back_end/open_api/service/OpenApiService.java
+++ back-end/src/main/java/com/onnongwa/back_end/open_api/service/OpenApiService.java
@@ -40,15 +40,29 @@ public class OpenApiService {
 				+ "- 출력 형식은 **key=value**, 속성별로 한 줄씩 반드시 작성합니다.\n"
 				+ "- ScheduleItems는 한 줄에 `시간-활동` 형태로 `;`로 구분하여 작성합니다.\n"
 				+ "- title, description, ScheduleItems 이 세가지만 리브랜딩해주고 나머지는 그대로 정돈만해줘!"
-				+ "- description 의 내용은 좀 더 풍부하게 해줘 500자 이하로"
+				+ "- description 의 내용은 좀 더 풍부하게 해줘 500자 이하로\n"
+				+ "- 무조건 예시의 양식대로 작성해줘\n"
+				+ "- 정리된 ScheduleItems을 토대로 총 체험이 몇시간 걸리는지 숫자만 적어줘 예) duration=3"
+				+ "- 온보딩 된 내용을 토대로 이 체험의 highlights 4가지 작성해줘 highlights는 ,로 분리해줘 예) 전문가와 함께하는 숲속 명상, 다도 체험 및 전통차 시음"
+				+ "- 온보딩된 내용을 토대로 이 체험의 inclusions도 1~3 가지 정도 작성해줘 inclusions는 ,로 분리해줘 예 ) 명상 도구 대여, 전문가 가이드"
+				+ "- 최종적으로 모든 내용을 가지고 hashtags를 추출해서 4가지 정도 작성해줘 hashtags는 ,로 분리해줘 예 ) #숲속명상, #차담, #힐링체험"
 				+ "예:\n"
 				+ "title=...\n"
 				+ "region=...\n"
 				+ "address=...\n"
+				+ "placeType=...\n"
+				+ "duration=...\n"
+				+ "regionType=...\n"
 				+ "description=...\n"
 				+ "crops=...\n"
 				+ "price=...\n"
 				+ "scheduleItems=...\n"
+				+ "highlights=... \n"
+				+ "inclusions=...\n"
+				+ "hashtags=...\n"
+				+ "startTime=...\n"
+				+ "endTime=...\n"
+				+ "selectedClosedDays=...\n"
 				+ "minParticipants=...\n"
 				+ "maxParticipants=...\n"
 				+ "hostName=...\n"
@@ -78,6 +92,8 @@ public class OpenApiService {
 			if (res == null || res.outputText() == null){
 				throw new RuntimeException("빈 응답");
 			}
+
+			System.out.println("결과 양식 \n" + res.outputText());
 
 			return ChatResponseParser.parse(res.outputText());
 		} catch(Exception e){
