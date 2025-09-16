736a5d811abb458aa3f6605721d3d04c91348172
jjsh0208 <jjsh0208@gmail.com>
2025-09-16 16:42:26 +0900

    feat : <Back> Experience 제목의 키워드 기준으로 검색하는 메서드 추가

--- back-end/src/main/java/com/onnongwa/back_end/domain/experience/elasticsearch/repository/ExperienceElasticRepository.java
+++ back-end/src/main/java/com/onnongwa/back_end/domain/experience/elasticsearch/repository/ExperienceElasticRepository.java
@@ -1,8 +1,11 @@
 package com.onnongwa.back_end.domain.experience.elasticsearch.repository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.onnongwa.back_end.domain.experience.elasticsearch.document.ExperienceDocument;
 
 public interface ExperienceElasticRepository extends ElasticsearchRepository<ExperienceDocument, Long> {
+	List<ExperienceDocument> findByTitle(String keyword);
 }
